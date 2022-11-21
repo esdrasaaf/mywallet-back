f9707c4dd899f9d10bce540c6b39dbd438ecb617
Esdras Antero <esdrasanterofontes@gmail.com>
2022-11-20 22:43:20 -0300

    alter port e script start

--- src/index.js
+++ src/index.js
@@ -17,4 +17,6 @@ app.use(authRouters)
 app.use(registersRouters)
 app.use(addsrRouters)
 
-app.listen(5000, console.log("Running on port 5000"))
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Running on port ${port}`))
